8e9d2e997212658f15c95d4561da3209d94d935f
Danielson77789 <Djp77789@gmail.com>
2024-04-13 14:13:59 -0700

    Update app.js

--- back-end/app.js
+++ back-end/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { user,score,game } = require('./models');
+const { Sequelize, Op } = require('sequelize');
+
 
 const app = express();
 app.use(express.json());
@@ -24,6 +26,31 @@ app.post('/submit', async (req,res) => {
     const { guessCount, bestScore, userId } = req.body;
     const scores = await score.create({ guessCount, bestScore, userId })
     res.json(scores);
+})
+
+app.post('/find-game', async (req,res) => {
+    const { day } = req.body; // Ensure 'day' is in 'YYYY-MM-DD' format
+
+    try {
+        const challenge = await game.findOne({
+            where: Sequelize.where(Sequelize.fn('date', Sequelize.col('createdAt')), '=', day)
+        });
+
+        if (challenge) {
+            res.json(challenge);
+        } else {
+            res.status(404).send('Game not found for the specified date.');
+        }
+    } catch (error) {
+        console.error('Search failed:', error);
+        res.status(500).send('An error occurred while searching for the game.');
+    }
+})
+
+app.post('/game', async (req,res) => {
+    const {goodOne, goodTwo, goodThree, goodFour, goodFive, badOne, badTwo, badThree, badFour, badFive} = req.body;
+    const newGame = game.create({goodOne, goodTwo, goodThree, goodFour, goodFive, badOne, badTwo, badThree, badFour, badFive})
+    res.send(newGame);
 })
 
 
